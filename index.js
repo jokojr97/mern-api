0c60f5a8ce61726643d8e741409ea7dc110e91b9
jokojr97 <joko.riyadi97@gmail.com>
2022-07-05 19:44:24 +0700

    update lesson 20 router node js

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express = require('express')
 
 const app = express();
 const port = 4000;
+const router = express.Router();
 
-app.use(() => {
-    console.log("hello server");
-    console.log("hello lagi");
+router.use('/products', (req, res, next) => {
+    // console.log("url: ", req.originalUrl);
+    // console.log("method: ", req.method);
+    res.json({
+        name: "joko riyadi",
+        email: "jokori@gmail.com"
+    });
+    next();
 })
+
+router.use('/price', (req, res, next) => {
+    res.json({
+        bareng: "komputer",
+        price: "150000"
+    });
+    next();
+})
+
+app.use('/', router);
 
 app.listen(port);
 
