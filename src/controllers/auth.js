9dfe15672435d20d7af09aec15df9756a8ed638e
jokojr97 <joko.riyadi97@gmail.com>
2022-10-06 19:44:04 +0700

    update backend login"

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,17 +1,109 @@
-exports.register = (req, res, next) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
+const { validationResult } = require('express-validator')
+const path = require('path');
+const fs = require('fs');
 
-    const result = {
-        message: "Register Success",
-        data: {
-            uid: 1,
-            name: name,
-            email: email,
-            password: password
-        }
+const User = require('../models/user');
+
+// exports.createUser = (req, res, next) => {
+
+//     const errors = validationResult(req);
+
+//     if (!errors.isEmpty()) {
+//         const err = new Error("invalid value")
+//         err.errorStatus = 400;
+//         err.data = errors.array();
+//         throw err;
+//     }
+
+//     const username = req.body.username;
+//     const email = req.body.email;
+//     const password = req.body.password;
+//     const image = "user.jpg"
+
+//     const insertUser = new User({
+//         email: email,
+//         username: username,
+//         password: password,
+//         image: image,
+//         level: {
+//             id: 1,
+//             level: "admin"
+//         },
+//     });
+
+//     insertUser.save()
+//         .then(result => {
+//             res.status(201).json({
+//                 message: "Create User Success",
+//                 data: result
+//             });
+//         }).catch(err => {
+//             console.log("err: ", err);
+//         });
+// }
+
+exports.register = (req, res, next) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const err = new Error("invalid value")
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
     }
 
-    res.status(201).json(result);
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const image = "user.jpg"
+
+    const insertUser = new User({
+        email: email,
+        username: username,
+        password: password,
+        image: image,
+        level: {
+            id: 1,
+            level: "admin"
+        },
+    });
+
+    insertUser.save()
+        .then(result => {
+            res.status(201).json({
+                message: "Register Success",
+                data: result
+            });
+        }).catch(err => {
+            console.log("err: ", err);
+        });
+}
+
+exports.login = (req, res, next) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const err = new Error("invalid value")
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+    const data = {
+        email: email,
+        password: password
+    }
+    User.findOne(data).then(result => {
+        res.status(200).json({
+            message: "Login Success!",
+            status: "login",
+            data: result
+        })
+    }).catch(err => {
+        next(err);
+    })
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,28 @@
 const express = require('express')
 
+const { body } = require('express-validator')
 const router = express.Router();
 
 const authController = require('../controllers/auth');
 
-router.post('/register', authController.register);
+// [POST] : /v1/auth/create
+// router.post('/create', [
+//     body('email').isEmail().withMessage("Format Email Tidak Sesuai"),
+//     body('password').isLength({ min: 5 }).withMessage("Password tidak sesuai minimal 5 karakter")],
+//     authController.createUser);
+
+
+// [POST] : /v1/auth/register
+router.post('/register', [
+    body('email').isEmail().withMessage("Format Email Tidak Sesuai"),
+    body('password').isLength({ min: 5 }).withMessage("Password tidak sesuai minimal 5 karakter")],
+    authController.register);
+
+
+// [POST] : /v1/auth/login
+router.post('/login', [
+    body('email').isEmail().withMessage("Format Email Tidak Sesuai"),
+    body('password').isLength({ min: 5 }).withMessage("Password tidak sesuai minimal 5 karakter")],
+    authController.login);
 
 module.exports = router;
